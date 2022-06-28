3b2dcd25df5149362441f02f77449654192d7417
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2022-06-27 22:42:55 -0300

    style: lint

--- src/main/java/com/autopeca/client/service/ClientService.java
+++ src/main/java/com/autopeca/client/service/ClientService.java
@@ -1,18 +1,18 @@
 package com.autopeca.client.service;
 
-import java.util.Random;
-
 import org.springframework.stereotype.Service;
 
 import com.autopeca.client.model.Client;
 
 @Service
 public class ClientService {
-	
+
 	public Client getClient(String id, String storeId){
 		Client client = new Client();
 		client.setId(id);
 		client.setStoreId(storeId);
+		client.setName("Haroldo");
+		client.setSurname("Vinicius");
 		client.setDescription("Cliente da filial de São Paulo");
 
 		return client;
@@ -20,7 +20,7 @@ public class ClientService {
 
 	public Client addClient(String storeId, String name, String surname){
 		Client client = new Client();
-		client.setId("654321");
+		client.setId("1234444");
 		client.setStoreId(storeId);
 		client.setName(name);
 		client.setSurname(surname);
