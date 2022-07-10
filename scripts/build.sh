9ad1d4a5ed702c94ef2283e51fb8719313c10333
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2022-07-10 01:15:59 -0300

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 echo "Creating local docker image for JAVA aplication"
 
 mvn package
-docker build -t inventory-server .
-docker tag inventory-server client-server:latest
+docker build -t client-server .
+docker tag client-server client-server:latest
 docker run -it --rm -p 8080:8080 --name client-server client-server:latest
