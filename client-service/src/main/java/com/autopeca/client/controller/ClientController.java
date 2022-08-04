b31a75abef43c59e45d3db7842afc36d5d2dbd4e
Ricardo Ferrari <ricardoaugusto.ferrari@gmail.com>
2022-08-03 23:51:45 -0300

    fix: circuit braker

--- client-service/src/main/java/com/autopeca/client/controller/ClientController.java
+++ client-service/src/main/java/com/autopeca/client/controller/ClientController.java
@@ -1,5 +1,8 @@
 package com.autopeca.client.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +25,27 @@ import org.apache.http.conn.HttpHostConnectException;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+
 @RestController
 @RequestMapping(value="v1/store/{storeId}/client")
 public class ClientController {
 
+	private static final String BACKEND = "clientService";
+	private static final java.util.logging.Logger logger = Logger.getLogger( ClientService.class.getName() );
+
 	@Autowired
 	private ClientService clientService;
 
+	// @RateLimiter(name = "balanceService", fallbackMethod = "fallbackRetrieveBalance")
+	// @Retry(name = "balanceService", fallbackMethod = "fallbackRetrieveBalance")
+	// @Bulkhead(name = "balanceService", type= Type.THREADPOOL, fallbackMethod = "fallbackRetrieveBalance")
+	@CircuitBreaker(name = BACKEND, fallbackMethod = "fallbackRetrieveBalance")
 	@RolesAllowed({"ADMIN", "EMPLOYEE"})
 	@GetMapping(value="/{clientId}")
 	public ResponseEntity<Client> getLicense( @PathVariable("storeId") String storeId,
@@ -39,6 +56,45 @@ public class ClientController {
 				linkTo(methodOn(ClientController.class).getLicense(storeId, clientId)).withSelfRel()
 			);
 	
+		return ResponseEntity.ok(client);
+	}
+
+	@SuppressWarnings("unused")
+	public ResponseEntity<Client> fallbackRetrieveBalance(String clientId, String storeId, CallNotPermittedException exception) {
+		Client client = clientService.getClientScoreOffline(clientId, storeId);
+		try {
+			client.add( 
+					linkTo(methodOn(ClientController.class).getLicense(storeId, clientId)).withSelfRel()
+				);
+		} catch (Exception e) {
+			logger.log(Level.WARNING,e.toString());
+		}
+		return ResponseEntity.ok(client);
+	}
+	
+	@SuppressWarnings("unused")
+	public ResponseEntity<Client> fallbackRetrieveBalance(String clientId, String storeId, IOException exception) {
+		Client client = clientService.getClientScoreOffline(clientId, storeId);
+		try {
+			client.add( 
+					linkTo(methodOn(ClientController.class).getLicense(storeId, clientId)).withSelfRel()
+				);
+		} catch (Exception e) {
+			logger.log(Level.WARNING,e.toString());
+		}
+		return ResponseEntity.ok(client);
+	}
+	
+	@SuppressWarnings("unused")
+	public ResponseEntity<Client> fallbackRetrieveBalance(String clientId, String storeId, TimeoutException exception) {
+		Client client = clientService.getClientScoreFailed(clientId, storeId);
+		try {
+			client.add( 
+					linkTo(methodOn(ClientController.class).getLicense(storeId, clientId)).withSelfRel()
+				);
+		} catch (Exception e) {
+			logger.log(Level.WARNING,e.toString());
+		}
 		return ResponseEntity.ok(client);
 	}
 	
--- client-service/src/main/java/com/autopeca/client/service/ClientService.java
+++ client-service/src/main/java/com/autopeca/client/service/ClientService.java
@@ -7,41 +7,30 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.autopeca.client.model.Client;
-import com.autopeca.client.service.adapter.BalanceRestTemplate;
 import com.autopeca.client.service.BalanceService;
 import com.autopeca.client.model.Balance;
 
-import java.util.Random;
-import java.security.SecureRandom;
 import java.util.concurrent.TimeoutException;
-import org.apache.http.conn.HttpHostConnectException;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
+import org.aspectj.lang.annotation.Aspect;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead.Type;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 
 @Service
+@Aspect
 public class ClientService {
-
-	@Autowired
-	BalanceRestTemplate balanceAdapter;
 
 	@Autowired
 	BalanceService balanceService;
 
-	private static final String BACKEND = "clientService";
-
 	private static final java.util.logging.Logger logger = Logger.getLogger( ClientService.class.getName() );
 
-	// @RateLimiter(name = "balanceService", fallbackMethod = "fallbackRetrieveBalance")
-	// @Retry(name = "balanceService", fallbackMethod = "fallbackRetrieveBalance")
-	// @Bulkhead(name = "balanceService", type= Type.THREADPOOL, fallbackMethod = "fallbackRetrieveBalance")
-	@CircuitBreaker(name = BACKEND, fallbackMethod = "fallbackRetrieveBalance")
-	public Client getClient(String id, String storeId) throws IOException, TimeoutException {
+	public Client getClient(String id, String storeId) throws TimeoutException, IOException, RuntimeException {
 		String firstName = "Andreza";
 		
 		Client client = new Client();
@@ -51,12 +40,7 @@ public class ClientService {
 		client.setSurname("Vinicius");
 		client.setDescription("Cliente da filial de São Paulo");
 		Balance balance = null;
-		try {
-			balance = balanceService.retrieveBalance(id);
-		} catch (IOException | TimeoutException e) {
-			logger.log(Level.WARNING,"*** Circuit braker almost open ***");
-			throw e;
-		}
+		balance = balanceService.retrieveBalance(id);
 
 		if (balance != null) {
 			client.setScore(balance.getScore());
@@ -65,15 +49,31 @@ public class ClientService {
 		}
 		return client;
 	}
+	
+	public Client getClientScoreOffline(String id, String storeId) {
+		String firstName = "Andreza";
+		
+		Client client = new Client();
+		client.setId(id);
+		client.setStoreId(storeId);
+		client.setName(firstName);
+		client.setSurname("Vinicius");
+		client.setDescription("Sem análise de crédito");
+		client.setScore(7);
+		return client;
+	}
 
-	@SuppressWarnings("unused")
-	private Balance fallbackRetrieveBalance(String clientId, Exception exception) {
-		Balance balance = new Balance();
-		balance.setClientId(clientId);
-		balance.setBalance(0.00);
-		balance.setScore(10);
-		balance.setAnnotation("Não foi possível obter a análise de crédito");
-		return balance;
+	public Client getClientScoreFailed(String id, String storeId) {
+		String firstName = "Andreza";
+		
+		Client client = new Client();
+		client.setId(id);
+		client.setStoreId(storeId);
+		client.setName(firstName);
+		client.setSurname("Vinicius");
+		client.setDescription("Falha na consulta de crédito");
+		client.setScore(7);
+		return client;
 	}
 
 	public Client addClient(String storeId, String name, String surname){
